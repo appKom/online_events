852678764c59b001fefa8042541c5dd9274680e5
Nexerate <erl1ls@outlook.com>
2023-09-20 11:49:50 +0200

    Added fonts and colors required to draw event card

--- lib/theme.dart
+++ lib/theme.dart
@@ -12,12 +12,26 @@ sealed class OnlineTheme {
   // static const purple = Color.fromRGBO(179, 107, 205, 1);
   // static const lightBlue = Color.fromRGBO(42, 198, 249, 1);
 
+  // Colors
   static const background = Color(0xFF000212); // ARGB
 
   static const gray11 = Color(0xFFDBDDE1);
   static const gray9 = Color(0xFFB7BBC3);
 
-  static const eventHeaderTextStyle = TextStyle(fontFamily: font, fontWeight: FontWeight.w500, fontSize: 16, )
-
+  // Fonts
   static const font = 'Poppins';
+
+  static const eventListHeader = TextStyle(
+    fontFamily: font,
+    fontWeight: FontWeight.w500,
+    fontSize: 16,
+    height: 24,
+  );
+
+  static const eventListSubHeader = TextStyle(
+    fontFamily: font,
+    fontWeight: FontWeight.w400,
+    fontSize: 12,
+    height: 18,
+  );
 }
