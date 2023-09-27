1d29cfaf501dad6873dcbeb4d546b1e43611bc5d
fredrir <fredrch@stud.ntnu.no>
2023-09-27 10:45:54 +0200

    New .dart file

---
+++ lib/bedpress.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import '/models/list_event.dart';
+import '/upcoming_card.dart';
+import '/theme.dart';
+
+
+class mainBedpress extends StatelessWidget{
+
+
+  
+}
+
+
+
+
+
+class upcomingBedpress<T> extends StatelessWidget{
+
+
+
+
+}
