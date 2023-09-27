de624481e7d7c03be9d87a7ecedd58bc12115d78
Nexerate <erl1ls@outlook.com>
2023-09-27 11:06:45 +0200

    Moved code into pages

---
+++ lib/pages/upcoming_events/upcoming_events_page.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+import 'upcoming_events.dart';
+import '/theme.dart';
+
+class UpcomingEventsPage extends StatelessWidget {
+  const UpcomingEventsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final padding = MediaQuery.of(context).padding + const EdgeInsets.symmetric(horizontal: 25);
+
+    return Material(
+      child: Padding(
+        padding: padding,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const SizedBox(height: 17),
+            SvgPicture.asset(
+              'assets/header.svg',
+              height: 48,
+            ),
+            const SizedBox(height: 60),
+            Expanded(
+              child: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    Container(
+                      height: 200,
+                      decoration: const BoxDecoration(
+                        color: OnlineTheme.white,
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(5),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 30),
+                    SizedBox(
+                      height: 276,
+                      child: UpcomingEventsList(
+                        models: testModels,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
