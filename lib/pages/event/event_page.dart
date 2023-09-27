4178f15ecc26841265594b573a3b3ffae3821678
Nexerate <erl1ls@outlook.com>
2023-09-27 11:14:14 +0200

    Added event page

---
+++ lib/pages/event/event_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class EventPage extends StatelessWidget {
+  const EventPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: Dette er din side Robin.
+    return Container();
+  }
+}
