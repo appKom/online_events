158e7e08fbec6a61bfcf356b8011a73d252c26a2
Nexerate <erl1ls@outlook.com>
2023-10-01 16:23:15 +0200

    Start work on event page shell

--- lib/pages/event/event_page.dart
+++ lib/pages/event/event_page.dart
@@ -1,11 +1,119 @@
 import 'package:flutter/material.dart';
+import 'package:online_events/theme.dart';
 
 class EventPage extends StatelessWidget {
   const EventPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // TODO: Dette er din side Robin.
-    return Container();
+    // final padding = MediaQuery.of(context).padding + const EdgeInsets.symmetric(horizontal: 40);
+
+    return Material(
+      color: OnlineTheme.background,
+      child: Column(
+        children: [
+          Container(
+            color: OnlineTheme.white,
+            height: 267,
+          ),
+          const Expanded(
+            child: Padding(
+              padding: EdgeInsets.only(top: 24, left: 40, right: 40),
+              child: SingleChildScrollView(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Text(
+                      'Surfetur til Portugal 2023',
+                      style: OnlineTheme.eventHeader,
+                    ),
+                    Text(
+                      'Av X-Sport',
+                      style: OnlineTheme.eventListSubHeader,
+                    ),
+                    EventDateCard(),
+                    EventParticipants(),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class EventDateCard extends StatelessWidget {
+  const EventDateCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(top: 24, bottom: 40),
+      height: 65,
+      decoration: const BoxDecoration(
+        color: OnlineTheme.blue1,
+        borderRadius: BorderRadius.all(
+          Radius.circular(15),
+        ),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+        child: Row(children: [
+          const SizedBox(
+            width: 27,
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 15,
+                  child: Text(
+                    'Sep',
+                    style: OnlineTheme.eventListSubHeader,
+                    overflow: TextOverflow.visible,
+                  ),
+                ),
+                Expanded(child: Text('24', style: OnlineTheme.eventCardDate)),
+              ],
+            ),
+          ),
+          Container(
+            width: 1,
+            height: 20,
+            color: OnlineTheme.gray8,
+            margin: const EdgeInsets.symmetric(horizontal: 30),
+          ),
+        ]),
+      ),
+    );
+  }
+}
+
+class EventParticipants extends StatelessWidget {
+  const EventParticipants({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 41,
+      child: Row(
+        children: [
+          Container(
+            width: 41,
+            height: 41,
+            decoration: const BoxDecoration(
+              color: OnlineTheme.blue1,
+              borderRadius: BorderRadius.all(
+                Radius.circular(10),
+              ),
+            ),
+            child: Icon(
+              Icons.person,
+              color: OnlineTheme.orange10,
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
--- lib/theme.dart
+++ lib/theme.dart
@@ -5,8 +5,14 @@ sealed class OnlineTheme {
   static const background = Color(0xFF000212); // ARGB
   static const white = Color(0xFFFFFFFF);
 
-  static const gray11 = Color(0xFFDBDDE1);
+  static const blue1 = Color(0xFF0D2546);
+  static const yellow = Color(0xFFFAB759);
+  static const orange10 = Color(0xFFFFEDB3); // More like yellow10
+
+  static const gray8 = Color(0xFFA6ABB5);
   static const gray9 = Color(0xFFB7BBC3);
+  static const gray10 = Color(0xFFC9CCD2);
+  static const gray11 = Color(0xFFDBDDE1);
   static const gray12 = Color(0xFFEDEEF0);
 
   // Fonts
@@ -27,6 +33,26 @@ sealed class OnlineTheme {
     color: gray9,
     fontWeight: FontWeight.w400,
     fontSize: 12,
+    height: 1.5,
+    fontStyle: FontStyle.normal,
+    decoration: TextDecoration.none,
+  );
+
+  static const eventHeader = TextStyle(
+    fontFamily: font,
+    color: white,
+    fontWeight: FontWeight.w700,
+    fontSize: 24,
+    height: 1.5,
+    fontStyle: FontStyle.normal,
+    decoration: TextDecoration.none,
+  );
+
+  static const eventCardDate = TextStyle(
+    fontFamily: font,
+    color: yellow,
+    fontWeight: FontWeight.w400,
+    fontSize: 20,
     height: 1.5,
     fontStyle: FontStyle.normal,
     decoration: TextDecoration.none,
