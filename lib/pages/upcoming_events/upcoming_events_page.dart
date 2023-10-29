d37b211465011a3d9a11fc98df93d50693c89ed7
Fredrir <fredrch@stud.ntnu.no>
2023-10-29 17:25:18 +0100

    moved bedpresscard

--- lib/pages/upcoming_events/upcoming_events_page.dart
+++ lib/pages/upcoming_events/upcoming_events_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
-import 'package:online_events/bedpress.dart';
+import 'package:online_events/pages/upcoming_events/bedpress.dart';
 import 'package:online_events/pages/upcoming_events/profile_button.dart';
 
 import 'upcoming_events.dart';
