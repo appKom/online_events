a66198b642dfc1266a9ffc222c390901818b461a
Fredrir <fredrch@stud.ntnu.no>
2023-10-30 14:31:01 +0100

    Added log inn page

---
+++ lib/pages/loginn/log_inn_page.dart
@@ -0,0 +1,167 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:online_events/pages/upcoming_events/profile_button.dart';
+import 'package:online_events/pages/upcoming_events/promoted_article.dart';
+import 'package:online_events/pages/upcoming_events/upcoming_events_page.dart';
+import 'package:online_events/theme.dart';
+
+import '../upcoming_events/bedpress.dart';
+import '../upcoming_events/upcoming_events.dart';
+
+class LogInnPage extends StatelessWidget {
+  const LogInnPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final padding = MediaQuery.of(context).padding + const EdgeInsets.symmetric(horizontal: 25);
+
+    return Material(
+      color: OnlineTheme.background,
+      child: Padding(
+        padding: padding,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const SizedBox(height: 17),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                GestureDetector(
+                  onTap: () {
+                    // Navigate to another page when the SVG image is tapped
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => const UpcomingEventsPage()), // Replace with your page class
+                    );
+                  },
+                  child: SvgPicture.asset(
+                    'assets/header.svg',
+                    height: 36,
+                    fit: BoxFit.fitHeight,
+                  ),
+                ),
+                const ProfileButton()
+              ],
+            ),
+            const SizedBox(height: 60),
+            Container(
+              width: 300,
+              height: 370,
+              padding: const EdgeInsets.only(bottom: 20),
+              margin: const EdgeInsets.only(right: 20),
+              child: Stack(
+                children: [
+                  const Positioned(
+                    left: 0,
+                    right: 0,
+                    top: 0,
+                    height: 111,
+                    child: Text(
+                      'Log inn to your Online Account',
+                      style: OnlineTheme.logInnPageHeader,
+                    )
+                  ),
+                  const Positioned(
+                      left: 15,
+                      top: 45,
+                      child: Text(
+                        'Email:',
+                        style: OnlineTheme.logInnPageEmail,
+                      )),
+                  const Positioned(
+                      left: 15,
+                      right: 15,
+                      top: 75,
+                      child: TextField(
+                        obscureText: false,
+                        style: OnlineTheme.logInnPageEmail,
+                        decoration: InputDecoration(
+                          filled: true,
+                          fillColor: OnlineTheme.gray14,
+                          hintText: 'Enter your Email',
+                          hintStyle: OnlineTheme.logInnPageInput,
+                          border: OutlineInputBorder(
+                            borderSide: BorderSide(color: OnlineTheme.gray15),
+                            
+                          ),
+                          enabledBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: OnlineTheme.gray15),
+                          )
+                        ),
+                      )),
+                  const Positioned(
+                      left: 15,
+                      top: 130,
+                      child: Text(
+                        'Password:',
+                        style: OnlineTheme.logInnPageEmail,
+                      )),
+                  const Positioned(
+                      left: 15,
+                      right: 15,
+                      top: 160,
+                      child: TextField(
+                        obscureText: true,
+                        style: OnlineTheme.logInnPageEmail,
+                        decoration: InputDecoration(
+                          filled: true,
+                          fillColor: OnlineTheme.gray14,
+                          hintText: 'Enter your password',
+                          hintStyle: OnlineTheme.logInnPageInput,
+                          border: OutlineInputBorder(
+                            borderSide: BorderSide(color: OnlineTheme.gray15),
+                            
+                          ),
+                          enabledBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: OnlineTheme.gray15),
+                          )
+                        ),
+                      )),
+                  Positioned(
+              left: 15,
+              right: 160,
+              top: 230,
+              height: 65,
+              child: Container(
+                decoration: BoxDecoration(
+                  color: OnlineTheme.green3,
+                  borderRadius: BorderRadius.circular(3),
+                ),
+                child: const Center(
+                  child: Text(
+                    'Log Inn',
+                    style: OnlineTheme.logInnPageButton,
+                  ),
+                ),
+              ),
+                  ),
+
+                  Positioned(
+              left: 160,
+              right: 15,
+              top: 230,
+              height: 65,
+              child: Container(
+                decoration: BoxDecoration(
+                  color: OnlineTheme.red1,
+                  borderRadius: BorderRadius.circular(3),
+                ),
+                child: const Center(
+                  child: Text(
+                    'Forgotten Password',
+                    style: OnlineTheme.logInnPageButton,
+                  ),
+                ),
+              ),
+                  ),
+                  
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
