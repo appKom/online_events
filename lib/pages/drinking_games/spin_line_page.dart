155d023bc0af8faba1a9eaa9a82950f62d74eead
Fredrir <fredrch@stud.ntnu.no>
2023-11-15 20:16:45 +0100

    Added spinline

---
+++ lib/pages/drinking_games/spin_line_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:online_events/components/online_scaffold.dart';
+import 'package:online_events/pages/drinking_games/spin_line.dart';
+
+class SpinLinePage extends StaticPage {
+  @override
+  Widget content(BuildContext context) {
+    // TODO: implement content
+    return const SpinLine();
+  }
+}
