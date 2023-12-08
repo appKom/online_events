27b77e5ffcf46e6982f1da1e3c17e6c4724dcdeb
Fredrir <fredrch@stud.ntnu.no>
2023-12-08 18:52:41 +0100

    Fixed MyEventsPage :)

--- lib/pages/events/my_events_page_loggedin.dart
+++ lib/pages/events/my_events_page_loggedin.dart
@@ -3,6 +3,7 @@ import 'package:online_events/components/animated_button.dart';
 import 'package:online_events/components/navbar.dart';
 import 'package:online_events/components/online_header.dart';
 import 'package:online_events/components/online_scaffold.dart';
+import 'package:online_events/core/models/event_model.dart';
 import 'package:online_events/pages/home/event_card_loggedin.dart';
 import 'package:online_events/pages/login/login_page.dart';
 import 'package:online_events/pages/profile/profile_page.dart';
@@ -42,9 +43,23 @@ class _MyEventsPageLoggedInState extends State<MyEventsPageLoggedIn> {
         const EdgeInsets.symmetric(horizontal: 25);
 
     final style = OnlineTheme.textStyle(size: 20, weight: 7);
-    final filteredEventModels = eventModels
+    final now = DateTime.now();
+
+    // Filter for upcoming registered events
+    final upcomingEvents = eventModels
         .where((model) => registeredEvents.contains(model.id))
-        .toList();
+        .where((model) {
+          final eventDate = DateTime.parse(model.startDate);
+          return eventDate.isAfter(now);
+        }).toList();
+
+    // Filter for past registered events
+    final pastEvents = eventModels
+        .where((model) => registeredEvents.contains(model.id))
+        .where((model) {
+          final eventDate = DateTime.parse(model.startDate);
+          return eventDate.isBefore(now);
+        }).toList();
 
     return SingleChildScrollView(
       child: Padding(
@@ -54,47 +69,33 @@ class _MyEventsPageLoggedInState extends State<MyEventsPageLoggedIn> {
           children: [
             SizedBox(height: OnlineHeader.height(context) + 40),
             Center(
-              child: Text(
-                'Mine Arrangementer',
-                style: style,
-              ),
+              child: Text('Mine Arrangementer', style: style),
             ),
-            Container(
-              margin: const EdgeInsets.symmetric(vertical: 10),
-              height: 111 * 1,
-              child: ListView.builder(
-                itemCount: filteredEventModels.length, // Update item count
-                padding: EdgeInsets.zero,
-                physics: const NeverScrollableScrollPhysics(),
-                itemBuilder: (c, i) => EventCardLoggedIn(
-                  model: filteredEventModels[i],
-                  attendeeInfoModel: attendeeInfoModels[
-                      i], // Assuming this list is aligned with filteredEventModels
-                  attendeeInfoModels: attendeeInfoModels,
-                ),
-              ),
-            ),
+            _buildEventList(upcomingEvents),
             const SizedBox(height: 24),
             Center(
-              child: Text(
-                'Tidligere Arrangementer',
-                style: style,
-              ),
+              child: Text('Tidligere Arrangementer', style: style),
             ),
-            Container(
-              margin: const EdgeInsets.symmetric(vertical: 10),
-              height: 111 * 6,
-              child: ListView.builder(
-                itemCount: 6,
-                padding: EdgeInsets.zero,
-                physics: const NeverScrollableScrollPhysics(),
-                itemBuilder: (c, i) => EventCardLoggedIn(
-                  model: eventModels[i], attendeeInfoModel: attendeeInfoModels[i], attendeeInfoModels: attendeeInfoModels,
-                ),
-              ),
-            ),
+            _buildEventList(pastEvents),
             SizedBox(height: Navbar.height(context)),
           ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildEventList(List<EventModel> events) {
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 10),
+      height: 111.0 * events.length,
+      child: ListView.builder(
+        itemCount: events.length,
+        padding: EdgeInsets.zero,
+        physics: const NeverScrollableScrollPhysics(),
+        itemBuilder: (context, index) => EventCardLoggedIn(
+          model: events[index],
+          attendeeInfoModel: attendeeInfoModels[index], // Ensure this list aligns with events
+          attendeeInfoModels: attendeeInfoModels,
         ),
       ),
     );
