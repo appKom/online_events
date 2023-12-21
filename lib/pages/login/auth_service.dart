ac131608b9234ededb10515046bcbf4e94960b3c
Fredrir <fhansteen@gmail.com>
2023-12-21 20:35:27 +0100

    error detectors

--- lib/pages/login/auth_service.dart
+++ lib/pages/login/auth_service.dart
@@ -2,28 +2,46 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 abstract class AuthService {
-  static const String clientId = '972717'; 
-  static const String redirectUri = 'https://cloud.appwrite.io/v1/account/sessions/oauth2/callback/oidc/65706141ead327e0436a'; // Replace with your Appwrite redirect URI
+  static const String clientId = '972717';
+  static const String redirectUri =
+      'https://cloud.appwrite.io/v1/account/sessions/oauth2/callback/oidc/65706141ead327e0436a'; // Replace with your Appwrite redirect URI
 
   static String get authorizationUrl =>
       'https://old.online.ntnu.no/openid/authorize?client_id=$clientId&redirect_uri=$redirectUri&response_type=code&scope=openid+profile+onlineweb4+events';
 
   static Future<Map<String, dynamic>?> exchangeCodeForToken(String code) async {
-    final response = await http.post(
-      Uri.parse('https://old.online.ntnu.no/openid/token'), 
-      headers: {'Content-Type': 'application/x-www-form-urlencoded'},
-      body: {
-        'grant_type': 'authorization_code',
-        'code': code,
-        'redirect_uri': redirectUri,
-        'client_id': clientId,
-      },
-    );
+    try {
+      final response = await http.post(
+        Uri.parse('https://old.online.ntnu.no/openid/token'),
+        headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+        body: {
+          'grant_type': 'authorization_code',
+          'code': code,
+          'redirect_uri': redirectUri,
+          'client_id': clientId,
+        },
+      );
 
-    if (response.statusCode == 200) {
-      return json.decode(response.body);
-    } else {
-      print('Error exchanging code for token: ${response.body}');
+      if (response.statusCode == 200) {
+        return json.decode(response.body);
+      } else {
+        // Log the error response for debugging purposes
+        print(
+            'Error exchanging code for token: ${response.statusCode} ${response.reasonPhrase} ${response.body}');
+        // Optionally, handle specific HTTP error codes if necessary
+        return null;
+      }
+    } on http.ClientException catch (e) {
+      // Handle client-side errors (e.g., network issues)
+      print('ClientException in exchangeCodeForToken: $e');
+      return null;
+    } on FormatException catch (e) {
+      // Handle JSON format errors
+      print('FormatException in exchangeCodeForToken: $e');
+      return null;
+    } catch (e) {
+      // Handle any other exceptions
+      print('Exception in exchangeCodeForToken: $e');
       return null;
     }
   }
