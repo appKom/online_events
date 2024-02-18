df5d16f172989a0b96c284be10f2e0aa3d835f24
Nexerate <erl1ls@outlook.com>
2024-02-18 19:13:39 +0100

    Improvements to roulette

--- lib/pages/games/roulette_page.dart
+++ lib/pages/games/roulette_page.dart
@@ -3,72 +3,53 @@ import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_fortune_wheel/flutter_fortune_wheel.dart';
-import 'package:online/dark_overlay.dart';
 
-import '../../theme/theme.dart';
+import '/dark_overlay.dart';
+import '/theme/theme.dart';
 
 class RoulettePage extends StatefulWidget {
-  const RoulettePage({Key? key}) : super(key: key);
+  const RoulettePage({super.key});
 
   @override
   State<RoulettePage> createState() => _RoulettePageState();
 }
 
 final challenges = [
-  {
-    "title": "Waterfall",
-    "description": "Alle drikker help til de til venstre slutter"
-  },
-  {
-    "title": "6 Minutes",
-    "description": "Finn en spotify playlist og folk må gjette sangene"
-  },
-  {
-    "title": "SHUG",
-    "description": "Drikk opp enhenten din",
-  },
-  {
-    "title": "Ratling Bog",
-    "description": "Sett på ratling bog",
-  },
-  {
-    "title": "Gi ut 10 slurker",
-    "description": "tittel",
-  },
-  {
-    "title": "Alle drikker",
-    "description": "Finn en spotify playlist og folk må gjette sangene"
-  },
-  {
-    "title": "Alle Single drikker",
-    "description": "Alle drikker help til de til venstre slutter"
-  },
-  {
-    "title": "Hot seat",
-    "description": "Finn en spotify playlist og folk må gjette sangene"
-  },
+  {"title": "Waterfall", "description": "Alle drikker help til de til venstre slutter"},
+  {"title": "6 Minutes", "description": "Finn en spotify playlist og folk må gjette sangene"},
+  {"title": "SHUG", "description": "Drikk opp enhenten din"},
+  {"title": "Ratling Bog", "description": "Sett på ratling bog"},
+  {"title": "Gi ut 10 slurker", "description": "tittel"},
+  {"title": "Alle drikker", "description": "Finn en spotify playlist og folk må gjette sangene"},
+  {"title": "Single drikker", "description": "Alle drikker help til de til venstre slutter"},
+  {"title": "Hot seat", "description": "Finn en spotify playlist og folk må gjette sangene"},
   {"title": "Shot", "description": "shot med den mest edrue i rommet"},
-  {
-    "title": "Fuck marry kill",
-    "description": "Finn en spotify playlist og folk må gjette sangene"
-  },
-  // ... other challenges
+  {"title": "Fuck marry kill", "description": "Finn en spotify playlist og folk må gjette sangene"},
+  {"title": "Waterfall", "description": "Alle drikker help til de til venstre slutter"},
+  {"title": "6 Minutes", "description": "Finn en spotify playlist og folk må gjette sangene"},
+  {"title": "SHUG", "description": "Drikk opp enhenten din"},
+  {"title": "Ratling Bog", "description": "Sett på ratling bog"},
+  {"title": "Gi ut 10 slurker", "description": "tittel"},
+  {"title": "Alle drikker", "description": "Finn en spotify playlist og folk må gjette sangene"},
+  {"title": "Single drikker", "description": "Alle drikker help til de til venstre slutter"},
+  {"title": "Hot seat", "description": "Finn en spotify playlist og folk må gjette sangene"},
+  {"title": "Shot", "description": "shot med den mest edrue i rommet"},
+  {"title": "Fuck marry kill", "description": "Finn en spotify playlist og folk må gjette sangene"},
 ];
 
 class _RoulettePageState extends State<RoulettePage> {
   final StreamController<int> _selected = StreamController<int>();
 
-  static final red = FortuneItemStyle(
+  static const red = FortuneItemStyle(
     color: OnlineTheme.red,
-    textStyle: OnlineTheme.textStyle(),
   );
-  static final green = FortuneItemStyle(
+
+  static const green = FortuneItemStyle(
     color: OnlineTheme.green,
-    textStyle: OnlineTheme.textStyle(),
   );
-  static final black = FortuneItemStyle(
-    color: Colors.black,
-    textStyle: OnlineTheme.textStyle(),
+
+  static const black = FortuneItemStyle(
+    color: OnlineTheme.gray13,
   );
 
   final fortuneList = List<FortuneItem>.empty(growable: true);
@@ -91,7 +72,7 @@ class _RoulettePageState extends State<RoulettePage> {
         FortuneItem(
           child: Text(
             challenges[i]['title']!,
-            style: OnlineTheme.textStyle(weight: 7),
+            style: OnlineTheme.header(),
           ),
           style: color,
         ),
@@ -104,29 +85,52 @@ class _RoulettePageState extends State<RoulettePage> {
 
   @override
   Widget build(BuildContext context) {
+    const size = 900.0;
+
     return Container(
       color: OnlineTheme.background,
       child: Center(
-        child: FortuneWheel(
-          onAnimationEnd: () {
-            RouletteOverlay(
-              title: challenges[index]["title"] ?? "",
-              description: challenges[index]["description"] ?? "",
-            ).show(context);
-          },
-          animateFirst: false,
-          selected: _selected.stream,
-          items: fortuneList,
-          physics: CircularPanPhysics(
-            duration: const Duration(seconds: 1),
-            curve: Curves.decelerate,
-          ),
-          onFling: () {
-            _selected.add(random.nextInt(challenges.length));
-          },
-          onFocusItemChanged: (value) {
-            index = value;
-          },
+        child: Stack(
+          alignment: Alignment.centerLeft,
+          children: [
+            Positioned(
+              width: size,
+              height: size,
+              left: -550,
+              child: ClipRect(
+                child: OverflowBox(
+                  maxWidth: size,
+                  maxHeight: size,
+                  child: Transform.rotate(
+                    angle: pi / 2,
+                    child: FortuneWheel(
+                      onAnimationEnd: () {
+                        RouletteOverlay(
+                          title: challenges[index]["title"] ?? "",
+                          description: challenges[index]["description"] ?? "",
+                        ).show(context);
+                      },
+                      alignment: Alignment.center,
+                      hapticImpact: HapticImpact.light,
+                      animateFirst: false,
+                      selected: _selected.stream,
+                      items: fortuneList,
+                      physics: CircularPanPhysics(
+                        duration: const Duration(seconds: 1),
+                        curve: Curves.decelerate,
+                      ),
+                      onFling: () {
+                        _selected.add(random.nextInt(challenges.length));
+                      },
+                      onFocusItemChanged: (value) {
+                        index = value;
+                      },
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
@@ -141,20 +145,22 @@ class RouletteOverlay extends DarkOverlay {
 
   @override
   Widget content(BuildContext context, Animation<double> animation) {
-    // TODO: implement content
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Text(
-          title,
-          style: OnlineTheme.header(),
-        ),
-        const SizedBox(height: 24),
-        Text(
-          description,
-          style: OnlineTheme.textStyle(),
-        ),
-      ],
+    return Padding(
+      padding: OnlineTheme.horizontalPadding,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            title,
+            style: OnlineTheme.header(),
+          ),
+          const SizedBox(height: 24),
+          Text(
+            description,
+            style: OnlineTheme.textStyle(),
+          ),
+        ],
+      ),
     );
   }
 }
