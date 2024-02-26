7a9e679498944c5aabba5a37cb61ca822747552e
Nexerate <erl1ls@outlook.com>
2024-02-26 20:11:08 +0100

    Fix transparency api crash on ios

--- lib/pages/games/songs/fader_abraham.dart
+++ lib/pages/games/songs/fader_abraham.dart
@@ -1,11 +1,59 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_markdown/flutter_markdown.dart';
 
 import '/components/online_scaffold.dart';
-import '/components/separator.dart';
 import '/theme/theme.dart';
 
 class FaderAbrahamPage extends ScrollablePage {
   const FaderAbrahamPage({super.key});
+
+  String _getText() {
+    return '''
+Fader abraham har fire sønner, ja fire sønner, har fader abraham! 
+Også drakk de litt (drikk)\n
+Også drakk de litt (drikk)\n
+Og de moret seg og sang; “Høyre arm”\n
+---
+Fader abraham har fire sønner, ja fire sønner, har fader abraham!\n
+Også drakk de litt (drikk)\n
+Også drakk de litt (drikk)\n
+Og de moret seg og sang; “Høyre arm, venstre arm”\n
+---
+I denne gamle staden satt så mangen en konge stor, og hadde nok av øl fra fat og piker ved sitt bord.
+Og de laga bøljer i gata når hjem ifra gildet de fór.
+Og nu sitter de alle mann alle i valhall og traller til oss i kor;\n
+---
+Fader abraham har fire sønner, ja fire sønner, har fader abraham!\n
+Også drakk de litt (drikk)\n
+Også drakk de litt (drikk)\n
+Og de moret seg og sang; “Høyre arm, venstre arm, høyre fot”\n
+---
+Fader abraham har fire sønner, ja fire sønner, har fader abraham!\n
+Også drakk de litt (drikk)\n
+Også drakk de litt (drikk)\n
+Og de moret seg og sang; “Høyre arm, venstre arm, høyre fot”\n
+---
+Fader abraham har fire sønner, ja fire sønner, har fader abraham!\n
+Også drakk de litt (drikk)\n
+Også drakk de litt (drikk)\n
+Og de moret seg og sang; “Høyre arm, venstre arm, høyre fot, venstre fot”\n
+---
+Fader abraham har fire sønner, ja fire sønner, har fader abraham!\n
+Også drakk de litt (drikk)\n
+Også drakk de litt (drikk)\n
+Og de moret seg og sang; “Høyre arm, venstre arm, høyre fot, venstre fot, rumpa ut”\n
+---
+Fader abraham har fire sønner, ja fire sønner, har fader abraham!\n
+Også drakk de litt (drikk)\n
+Også drakk de litt (drikk)\n
+Og de moret seg og sang; “Høyre arm, venstre arm, høyre fot, venstre fot, rumpa ut, kroppen frem”\n
+---
+Fader abraham har fire sønner, ja fire sønner, har fader abraham!\n
+Også drakk de litt (drikk)\n
+Også drakk de litt (drikk)\n
+Og de moret seg og sang; “Høyre arm, venstre arm, høyre fot, venstre fot, rumpa ut, kroppen frem, tunga ut”\n
+''';
+  }
 
   @override
   Widget content(BuildContext context) {
@@ -28,56 +76,28 @@ class FaderAbrahamPage extends ScrollablePage {
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.stretch,
               children: [
-                const SizedBox(height: 20),
+                const SizedBox(height: 24),
                 Text(
                   'Fader Abraham',
                   style: OnlineTheme.header(),
                 ),
-                const Separator(margin: 5),
-                const SizedBox(height: 5),
-                Text(
-                  'Fader abraham har fire sønner, ja fire sønner, har fader abraham! Også drakk de litt (drikk.) Også drakk de litt (drikk.) Og de moret seg og sang; “Høyre arm”',
-                  style: OnlineTheme.textStyle(),
-                ),
-                const SizedBox(height: 8),
-                Text(
-                  'Fader abraham har fire sønner, ja fire sønner, har fader abraham! Også drakk de litt (drikk.) Også drakk de litt (drikk.) Og de moret seg og sang; “Høyre arm, venstre arm”',
-                  style: OnlineTheme.textStyle(),
-                ),
-                const SizedBox(height: 8),
-                Text(
-                  'I denne gamle staden satt så mangen en konge stor, og hadde nok av øl fra fat og piker ved sitt bord. Og de laga bøljer i gata når hjem ifra gildet de fór. Og nu sitter de alle mann alle i valhall og traller til oss i kor;',
-                  style: OnlineTheme.textStyle(),
-                ),
-                const SizedBox(height: 8),
-                Text(
-                  'Fader abraham har fire sønner, ja fire sønner, har fader abraham! Også drakk de litt (drikk.) Også drakk de litt (drikk.) Og de moret seg og sang; “Høyre arm, venstre arm, høyre fot”',
-                  style: OnlineTheme.textStyle(),
-                ),
-                const SizedBox(height: 8),
-                Text(
-                  'Fader abraham har fire sønner, ja fire sønner, har fader abraham! Også drakk de litt (drikk.) Også drakk de litt (drikk.) Og de moret seg og sang; “Høyre arm, venstre arm, høyre fot”',
-                  style: OnlineTheme.textStyle(),
-                ),
-                const SizedBox(height: 8),
-                Text(
-                  'Fader abraham har fire sønner, ja fire sønner, har fader abraham! Også drakk de litt (drikk.) Også drakk de litt (drikk.) Og de moret seg og sang; “Høyre arm, venstre arm, høyre fot, venstre fot”',
-                  style: OnlineTheme.textStyle(),
-                ),
-                const SizedBox(height: 8),
-                Text(
-                  'Fader abraham har fire sønner, ja fire sønner, har fader abraham! Også drakk de litt (drikk.) Også drakk de litt (drikk.) Og de moret seg og sang; “Høyre arm, venstre arm, høyre fot, venstre fot, rumpa ut”',
-                  style: OnlineTheme.textStyle(),
-                ),
-                const SizedBox(height: 8),
-                Text(
-                  'Fader abraham har fire sønner, ja fire sønner, har fader abraham! Også drakk de litt (drikk.) Også drakk de litt (drikk.) Og de moret seg og sang; “Høyre arm, venstre arm, høyre fot, venstre fot, rumpa ut, kroppen frem”',
-                  style: OnlineTheme.textStyle(),
-                ),
-                const SizedBox(height: 8),
-                Text(
-                  'Fader abraham har fire sønner, ja fire sønner, har fader abraham! Også drakk de litt (drikk.) Også drakk de litt (drikk.) Og de moret seg og sang; “Høyre arm, venstre arm, høyre fot, venstre fot, rumpa ut, kroppen frem, tunga ut”',
-                  style: OnlineTheme.textStyle(),
+                const SizedBox(height: 24),
+                MarkdownBody(
+                  data: _getText(),
+                  styleSheet: MarkdownStyleSheet.fromTheme(Theme.of(context)).copyWith(
+                    p: OnlineTheme.textStyle(),
+                    h1: const TextStyle(color: OnlineTheme.white),
+                    h2: const TextStyle(color: OnlineTheme.white),
+                    h3: const TextStyle(color: OnlineTheme.white),
+                    h4: const TextStyle(color: OnlineTheme.white),
+                    h5: const TextStyle(color: OnlineTheme.white),
+                    h6: const TextStyle(color: OnlineTheme.white),
+                    horizontalRuleDecoration: const BoxDecoration(
+                      border: Border(
+                        top: BorderSide(width: 1, color: OnlineTheme.grayBorder),
+                      ),
+                    ),
+                  ),
                 ),
                 const SizedBox(height: 24),
               ],
--- lib/pages/login/login_page.dart
+++ lib/pages/login/login_page.dart
@@ -1,93 +1,66 @@
 import 'package:app_tracking_transparency/app_tracking_transparency.dart';
 import 'package:flutter/material.dart';
 
-import '../../services/app_navigator.dart';
 import '/components/animated_button.dart';
+import '/components/online_scaffold.dart';
+import '/services/app_navigator.dart';
 import '/theme/theme.dart';
 import 'auth_web_view_page.dart';
 
-class LoginPage extends StatefulWidget {
+class LoginPage extends StaticPage {
   const LoginPage({super.key});
 
   @override
-  LoginPageState createState() => LoginPageState();
-}
+  Widget content(BuildContext context) {
+    final padding = MediaQuery.of(context).padding + OnlineTheme.horizontalPadding;
+    final isIos = Theme.of(context).platform == TargetPlatform.iOS;
 
-class LoginPageState extends State<LoginPage> {
-  bool _isIOS = false;
-  bool _permissionRequested = false;
+    return FutureBuilder(
+      future: appTrackingPermission(isIos),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState != ConnectionState.done) return const IgnorePointer();
 
-  @override
-  void initState() {
-    super.initState();
-  }
-
-  @override
-  void didChangeDependencies() {
-    super.didChangeDependencies();
-    if (!_permissionRequested) {
-      _isIOS = Theme.of(context).platform == TargetPlatform.iOS;
-      if (_isIOS) {
-        appTrackingPermission();
-        _permissionRequested = true;
-      }
-    }
-  }
-
-  Future<void> appTrackingPermission() async {
-    final TrackingStatus status =
-        await AppTrackingTransparency.trackingAuthorizationStatus;
-
-    if (status == TrackingStatus.notDetermined) {
-      await AppTrackingTransparency.requestTrackingAuthorization();
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    final padding =
-        MediaQuery.of(context).padding + OnlineTheme.horizontalPadding;
-
-    final headerStyle = OnlineTheme.textStyle(
-      size: 20,
-      weight: 7,
+        return Padding(
+          padding: EdgeInsets.only(left: padding.left, right: padding.right),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('Logg inn med Online', style: OnlineTheme.header()),
+              const SizedBox(height: 24),
+              AnimatedButton(
+                onTap: openLoginWebView,
+                childBuilder: (context, hover, pointerDown) {
+                  return Container(
+                    height: OnlineTheme.buttonHeight,
+                    alignment: Alignment.center,
+                    decoration: BoxDecoration(
+                      color: OnlineTheme.green.withOpacity(0.4),
+                      borderRadius: BorderRadius.circular(5),
+                      border: const Border.fromBorderSide(BorderSide(color: OnlineTheme.green, width: 2)),
+                    ),
+                    child: Text(
+                      'Logg Inn',
+                      style: OnlineTheme.textStyle(color: OnlineTheme.green, weight: 5),
+                    ),
+                  );
+                },
+              )
+            ],
+          ),
+        );
+      },
     );
+  }
 
-    return Scaffold(
-      backgroundColor: OnlineTheme.background,
-      body: Padding(
-        padding: EdgeInsets.only(left: padding.left, right: padding.right),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text('Logg inn med Online', style: headerStyle),
-            const SizedBox(height: 24),
-            AnimatedButton(
-              onTap: () {
-                AppNavigator.navigateToPage(const LoginWebView());
-              },
-              childBuilder: (context, hover, pointerDown) {
-                return Container(
-                  height: OnlineTheme.buttonHeight,
-                  alignment: Alignment.center,
-                  decoration: BoxDecoration(
-                    color: OnlineTheme.green.withOpacity(0.4),
-                    borderRadius: BorderRadius.circular(5),
-                    border: const Border.fromBorderSide(
-                        BorderSide(color: OnlineTheme.green, width: 2)),
-                  ),
-                  child: Text(
-                    'Logg Inn',
-                    style: OnlineTheme.textStyle(
-                        color: OnlineTheme.green, weight: 5),
-                  ),
-                );
-              },
-            )
-          ],
-        ),
-      ),
-    );
+  Future<void> appTrackingPermission(bool isIos) async {
+    if (!isIos) return;
+
+    await AppTrackingTransparency.requestTrackingAuthorization();
+    // final TrackingStatus status = await AppTrackingTransparency.trackingAuthorizationStatus;
+  }
+
+  void openLoginWebView() {
+    AppNavigator.navigateToPage(const LoginWebView());
   }
 }
