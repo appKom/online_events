4dc09c87d05d842c540926285d94ef2123f0cf13
fredrir <fhansteen@gmail.com>
2024-03-03 15:52:51 +0100

    håper Erlend liker knappen

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -9,13 +9,15 @@ import '/services/app_navigator.dart';
 import '/theme/theme.dart';
 import 'article_carousel.dart';
 import 'bedpres.dart';
+import 'info_page.dart';
 
 class HomePage extends ScrollablePage {
   const HomePage({super.key});
 
   @override
   Widget content(BuildContext context) {
-    final padding = MediaQuery.of(context).padding + OnlineTheme.horizontalPadding;
+    final padding =
+        MediaQuery.of(context).padding + OnlineTheme.horizontalPadding;
 
     return Padding(
       padding: padding,
@@ -113,8 +115,35 @@ class HomePage extends ScrollablePage {
           ValueListenableBuilder(
             valueListenable: Client.articlesCache,
             builder: (context, articles, child) {
-              if (articles.isEmpty) return Center(child: ArticleCarousel.skeleton(context));
-              return Center(child: ArticleCarousel(articles: articles.take(3).toList()));
+              if (articles.isEmpty)
+                return Center(child: ArticleCarousel.skeleton(context));
+              return Center(
+                  child: ArticleCarousel(articles: articles.take(3).toList()));
+            },
+          ),
+          const SizedBox(height: 24 + 24),
+          AnimatedButton(
+            onTap: () {
+              AppNavigator.navigateToPage(const InfoPage());
+            },
+            childBuilder: (context, hover, pointerDown) {
+              return Container(
+                height: 40,
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                  color: OnlineTheme.yellow.withOpacity(0.4),
+                  borderRadius: BorderRadius.circular(5.0),
+                  border: const Border.fromBorderSide(
+                      BorderSide(color: OnlineTheme.yellow, width: 2)),
+                ),
+                child: Text(
+                  'Om Online-Appen',
+                  style: OnlineTheme.textStyle(
+                    weight: 5,
+                    color: OnlineTheme.yellow,
+                  ),
+                ),
+              );
             },
           ),
           const SizedBox(height: 24),
