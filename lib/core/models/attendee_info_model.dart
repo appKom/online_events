fec3be9eeff7e19a2c8f7ed00db28c611620f1c5
fredrir <fhansteen@gmail.com>
2024-03-26 00:59:03 +0100

    attendeInfoModel fix for marked users

--- lib/core/models/attendee_info_model.dart
+++ lib/core/models/attendee_info_model.dart
@@ -10,13 +10,7 @@ class AttendeeInfoModel {
   final List<int> ruleBundles;
   final int numberOnWaitlist;
   final int numberOfSeatsTaken;
-  final bool hasFeedback;
-  final bool hasExtras;
-  final bool hasReservation;
-  final List<dynamic> extras; // Replace dynamic with specific type if needed
-  final dynamic payment; // Replace dynamic with specific type if needed
-  final dynamic feedback; // Replace dynamic with specific type if needed
-  final bool hasPostponedRegistration;
+  final String hasPostponedRegistration;
   final bool isMarked;
   final bool isSuspended;
   final Eligibility isEligibleForSignup;
@@ -31,19 +25,15 @@ class AttendeeInfoModel {
       waitlist: false,
       guestAttendance: false,
       registrationStart: DateTime.now(),
-      registrationEnd: DateTime.now().add(const Duration(days: 30)), // Default end date 30 days from now
-      unattendDeadline: DateTime.now().add(const Duration(days: 10)), // Default deadline 10 days from now
+      registrationEnd: DateTime.now()
+          .add(const Duration(days: 30)), // Default end date 30 days from now
+      unattendDeadline: DateTime.now()
+          .add(const Duration(days: 10)), // Default deadline 10 days from now
       automaticallySetMarks: false,
       ruleBundles: [],
       numberOnWaitlist: 0,
       numberOfSeatsTaken: 0,
-      hasFeedback: false,
-      hasExtras: false,
-      hasReservation: false,
-      extras: [],
-      payment: null, // Assuming null is a valid default
-      feedback: null, // Assuming null is a valid default
-      hasPostponedRegistration: false,
+      hasPostponedRegistration: '',
       isMarked: false,
       isSuspended: false,
       isEligibleForSignup: Eligibility.withDefaults(),
@@ -65,12 +55,6 @@ class AttendeeInfoModel {
     required this.ruleBundles,
     required this.numberOnWaitlist,
     required this.numberOfSeatsTaken,
-    required this.hasFeedback,
-    required this.hasExtras,
-    required this.hasReservation,
-    required this.extras,
-    this.payment,
-    this.feedback,
     required this.hasPostponedRegistration,
     required this.isMarked,
     required this.isSuspended,
@@ -86,24 +70,26 @@ class AttendeeInfoModel {
       maxCapacity: json['max_capacity'] ?? 0,
       waitlist: json['waitlist'] ?? false,
       guestAttendance: json['guest_attendance'] ?? false,
-      registrationStart:
-          json['registration_start'] != null ? DateTime.parse(json['registration_start']) : DateTime.now(),
-      registrationEnd: json['registration_end'] != null ? DateTime.parse(json['registration_end']) : DateTime.now(),
-      unattendDeadline: json['unattend_deadline'] != null ? DateTime.parse(json['unattend_deadline']) : DateTime.now(),
+      registrationStart: json['registration_start'] != null
+          ? DateTime.parse(json['registration_start'])
+          : DateTime.now(),
+      registrationEnd: json['registration_end'] != null
+          ? DateTime.parse(json['registration_end'])
+          : DateTime.now(),
+      unattendDeadline: json['unattend_deadline'] != null
+          ? DateTime.parse(json['unattend_deadline'])
+          : DateTime.now(),
       automaticallySetMarks: json['automatically_set_marks'] ?? false,
-      ruleBundles: json['rule_bundles'] != null ? List<int>.from(json['rule_bundles']) : [],
+      ruleBundles: json['rule_bundles'] != null
+          ? List<int>.from(json['rule_bundles'])
+          : [],
       numberOnWaitlist: json['number_on_waitlist'] ?? 0,
       numberOfSeatsTaken: json['number_of_seats_taken'] ?? 0,
-      hasFeedback: json['has_feedback'] ?? false,
-      hasExtras: json['has_extras'] ?? false,
-      hasReservation: json['has_reservation'] ?? false,
-      extras: json['extras'] ?? [],
-      payment: json['payment'],
-      feedback: json['feedback'],
       hasPostponedRegistration: json['has_postponed_registration'] ?? false,
       isMarked: json['is_marked'] ?? false,
       isSuspended: json['is_suspended'] ?? false,
-      isEligibleForSignup: Eligibility.fromJson(json['is_eligible_for_signup'] ?? {}),
+      isEligibleForSignup:
+          Eligibility.fromJson(json['is_eligible_for_signup'] ?? {}),
       isAttendee: json['is_attendee'] ?? false,
       isOnWaitlist: json['is_on_waitlist'] ?? false,
       whatPlaceIsUserOnWaitList: json['what_place_is_user_on_wait_list'] ?? 0,
